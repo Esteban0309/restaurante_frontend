fe12d00a55cfbc543a3f702ce3e20f2008aadf8e
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-07-08 21:30:24 -0500

    .

---
+++ env.ts
@@ -0,0 +1,8 @@
+DB_HOST=localhost
+DB_PORT=5432
+DB_USERNAME=postgres
+DB_PASSWORD=postgres
+DB_NAME=neondb
+
+# MONGO_URI=mongodb+srv://corellaesteban11:esteban11@cluster0esteban.q7q64xv.mongodb.net/restaurante?retryWrite=true&w=majority
+MONGO_URI=mongodb://localhost:27017/restaurante
